32b73ef36af2de67e2e4f29c494692e52e62fd96
raj21d <rajad211207@gmail.com>
2024-05-27 19:44:04 +0530

    vehicalhealthmonitoring

--- newapi1.js
+++ newapi1.js
@@ -11,22 +11,22 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 // TODO ENDPOINTS
 app.post('/Register', async(req, res) => {
-    const  {holdername,vehicalnumber,chassisnumber,insurancenumber,licensenumber}= req.body;
+    const  {holdername,vehiclenumber,chassisnumber,insurancenumber,licensenumber}= req.body;
 
     try{
-        await registerdetails.create( {holdername,vehicalnumber,chassisnumber,insurancenumber,licensenumber});
-        res.status(200).send('Vehical registered successfully');
+        await registerdetails.create( {holdername,vehiclenumber,chassisnumber,insurancenumber,licensenumber});
+        res.status(200).send('Vehicle registered successfully');
     }catch(err){
         console.error(err)
-        res.status(404).send('Vehical already registered')
+        res.status(404).send('Vehicle already registered')
     }
     
 });
 
 app.post('/status',async (req, res) => {
-    const  {vehicalnumber,engine,brake,healthstatus}= req.body;
+    const  {vehiclenumber,engine,brake,healthstatus}= req.body;
     try{
-        await trackStatus.findOneAndUpdate({vehicalnumber},{engine,brake,healthstatus},{upsert:true});
+        await trackStatus.findOneAndUpdate({vehiclenumber},{engine,brake,healthstatus},{upsert:true});
         res.status(200).send('Status updated sucessfully');
     }catch(err){
         console.error(err);
@@ -36,17 +36,17 @@ app.post('/status',async (req, res) => {
 });
 
 app.get('/status',async (req, res) => {
-    const {vehicalnumber}=req.query;
+    const {vehiclenumber}=req.query;
     try{
-        const Vehical=await trackStatus.findOne({vehicalnumber});
-        if(Vehical){
-            res.status(200).json(Vehical);
+        const vehicle=await trackStatus.findOne({vehiclenumber});
+        if(vehicle){
+            res.status(200).json(vehicle);
         }else{
-            res.status(404).send('Vehical not found')
+            res.status(404).send('Vehicle not found')
         }
     }catch(err){
         console.error(err);
-        res.status(500).send ('Failed to find vehical');
+        res.status(500).send ('Failed to find vehicle');
     }
 });
 
