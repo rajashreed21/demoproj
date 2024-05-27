f756aa10ebe549f5f4f124afb10e93e567e8abba
raj21d <rajad211207@gmail.com>
2024-05-27 15:03:34 +0530

    vehicalhealthmonitoring

--- newapi1.js
+++ newapi1.js
@@ -2,7 +2,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const { registerdetails, trackStatus } = require('./db/user');
 const app = express()
-const PORT = process.env.PORT || 3005;
+const PORT = process.env.PORT || 3006;
 
 //Middlewares
 app.use(bodyParser.json())
@@ -10,14 +10,15 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 let Vehicals ={};
 // TODO ENDPOINTS
-app.post('/Register', (req, res) => {
+app.post('/Register', async(req, res) => {
     const  {holdername,vehicalnumber,chassisnumber,insurancenumber,licensenumber}= req.body;
 
-    if(Vehicals[vehicalnumber]){
-        res.status(409).send('Vehical already registered');
-    }else{
-        Vehicals[vehicalnumber]={holdername,chassisnumber,insurancenumber,licensenumber};
-        res.status(200).send('Vehical registered successfully')
+    try{
+        await registerdetails.create( {holdername,vehicalnumber,chassisnumber,insurancenumber,licensenumber});
+        res.status(200).send('Vehical registered successfully');
+    }catch(err){
+        console.error(err)
+        res.status(404).send('Vehical already registered')
     }
     
 });
@@ -26,35 +27,30 @@ app.get("/", (req, res) => {
     res.end("Hello world")
 })
 
-app.post('/status', (req, res) => {
+app.post('/status',async (req, res) => {
     const  {vehicalnumber,engine,brake,healthstatus}= req.body;
-    if (Vehicals[vehicalnumber]){
-        Vehicals[vehicalnumber].engine=engine;
-        Vehicals[vehicalnumber].brake=brake;
-        Vehicals[vehicalnumber].healthstatus=healthstatus;
+    try{
+        await trackStatus.findOneAndUpdate({vehicalnumber},{engine,brake,healthstatus},{upsert:true});
         res.status(200).send('Status updated sucessfully');
-    }else{
+    }catch(err){
+        console.error(err);
         res.status(404).send('Vehical not found')
     }
         
 });
 
-app.get('/status', (req, res) => {
+app.get('/status',async (req, res) => {
     const {vehicalnumber}=req.query;
-    if(Vehicals[vehicalnumber]){
-        const{holdername,chassisnumber,insurancenumber,licensenumber,engine,brake,healthstatus}=Vehicals[vehicalnumber];
-        res.status(200).json({
-            holdername,
-            vehicalnumber,
-            chassisnumber,
-            insurancenumber,
-            licensenumber,
-            engine,
-            brake,
-            healthstatus
-        });
-    }else{
-        res.status(404).send ('Vehical not found');
+    try{
+        const Vehical=await trackStatus.findOne({vehicalnumber});
+        if(Vehical){
+            res.status(200).json(Vehical);
+        }else{
+            res.status(404).send('Vehical not found')
+        }
+    }catch(err){
+        console.error(err);
+        res.status(500).send ('Failed to find vehical');
     }
 });
 
